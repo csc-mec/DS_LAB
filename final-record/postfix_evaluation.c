41baa1f66ccbcdadf87f6966e055d34df21f82ee
n1ved <nivedrsalini@gmail.com>
2023-12-27 20:07:47 +0530

    Add postfix Evaluation

---
+++ final-record/postfix_evaluation.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define Msize 100
+int top = -1;
+int S[Msize];
+void push(int x)
+{
+    if (top != Msize - 1)
+    {
+        top = top + 1;
+        S[top] = x;
+    }
+}
+int pop()
+{
+    if (top != -1)
+    {
+        top = top - 1;
+        return S[top + 1];
+    }
+}
+bool isOperator(char ch)
+{
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+}
+int main()
+{
+    char C[30];
+    int i = 0, o1, o2, r;
+    printf("Enter expression : ");
+    scanf("%s", &C);
+    int l = strlen(C);
+    while (i != l)
+    {
+        if (!isOperator(C[i]))
+        {
+            int n = C[i] - '0';
+            push(n);
+        }
+        else
+        {
+            o2 = pop();
+            o1 = pop();
+            switch (C[i])
+            {
+            case '+':
+                r = o1 + o2;
+                break;
+            case '-':
+                r = o1 - o2;
+                break;
+            case '*':
+                r = o1 * o2;
+                break;
+            case '/':
+                r = o1 / o2;
+                break;
+            case '^':
+                r = 1;
+                for (int k = 1; k <= o2; k++)
+                {
+                    r = r * o1;
+                }
+                break;
+            default:
+                printf("Invalid operators\n");
+                return 0;
+            }
+            push(r);
+        }
+        i = i + 1;
+    }
+    int result = pop();
+    printf("Result is : %d\n", result);
+}
