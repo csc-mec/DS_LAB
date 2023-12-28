9e7b38dce647c9bdd1436d81f5a7f46f7b06a320
n1ved <nivedrsalini@gmail.com>
2023-12-28 22:08:42 +0530

    Added Hasing

---
+++ final-record/hashing.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct HashTable
+{
+    struct Node *chain[SIZE];
+};
+
+void init(struct HashTable *ht)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        ht->chain[i] = NULL;
+    }
+}
+
+void insert(struct HashTable *ht, int value)
+{
+    int key = value % SIZE;
+
+    struct Node *newNode = malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (ht->chain[key] == NULL)
+    {
+        ht->chain[key] = newNode;
+    }
+
+    else
+    {
+        struct Node *temp = ht->chain[key];
+
+        while (temp->next)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    printf("Entered value %d added to hash table\n", value);
+}
+
+void printHashTable(struct HashTable *ht)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("chain[%d]-->", i);
+        struct Node *temp = ht->chain[i];
+
+        while (temp)
+        {
+            printf("%d -->", temp->data);
+            temp = temp->next;
+        }
+
+        printf("NULL\n");
+    }
+}
+
+void main()
+{
+    struct HashTable ht;
+    init(&ht);
+
+    int num, val;
+    printf("Enter the number of elements: ");
+    scanf("%d", &num);
+
+    for (int i = 0; i < num; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &val);
+        insert(&ht, val);
+    }
+
+    printf("\n\nThe hash table is:\n\n");
+    printHashTable(&ht);
+}
